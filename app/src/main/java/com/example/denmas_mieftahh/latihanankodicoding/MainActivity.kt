16c5ded592049eb018505103c60e8f41917f0768
Miftahul93 <miftahululum693@gmail.com>
2018-10-23 22:05:08 +0700

    Membuat class untuk menampung semua componen

--- app/src/main/java/com/example/denmas_mieftahh/latihanankodicoding/MainActivity.kt
+++ app/src/main/java/com/example/denmas_mieftahh/latihanankodicoding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.denmas_mieftahh.latihanankodicoding
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MainActivityUI().setContentView(this)
+    }
+
+    class MainActivityUI : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                padding = dip(16)
+
+                val name = editText {
+                    hint = "What's your name ??"
+                }
+            }
+        }
     }
 }
