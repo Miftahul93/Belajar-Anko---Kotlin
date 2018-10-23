780eb6862a95f9f355ae5e6869b166d2038bb871
Miftahul93 <miftahululum693@gmail.com>
2018-10-23 22:07:22 +0700

    Menambahkan Show Button Toast

--- app/src/main/java/com/example/denmas_mieftahh/latihanankodicoding/MainActivity.kt
+++ app/src/main/java/com/example/denmas_mieftahh/latihanankodicoding/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.denmas_mieftahh.latihanankodicoding
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import com.example.denmas_mieftahh.latihanankodicoding.R.color.colorAccent
 import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,16 @@ class MainActivity : AppCompatActivity() {
 
                 val name = editText {
                     hint = "What's your name ??"
+                }
+
+                //Button Toast
+                button("Say Hello") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick { toast("Hello, ${name.text}!") }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
                 }
             }
         }
